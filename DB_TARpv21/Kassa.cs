171eef67a35b7a6aaf310a48cd589f1420caf006
marina.oleinik <marina.oleinik@HP-CZC6297B9D>
2022-11-11 15:17:36 +0200

    Kassas on tooded kategooriate jargi

--- DB_TARpv21/Kassa.cs
+++ DB_TARpv21/Kassa.cs
@@ -17,6 +17,8 @@ namespace DB_TARpv21
         SqlCommand cmd;
         SqlDataAdapter adapter_toode, adapter_kat;
         TabControl kategooriad;
+        SqlDataAdapter failinimi_adap;
+        PictureBox pictureBox;
         public Kassa()
         {
             this.Size = new System.Drawing.Size(600, 300);
@@ -27,24 +29,41 @@ namespace DB_TARpv21
         }
         TableLayoutPanel tlp;
 
-        
 
-        //public void Tooded()
-        //{
-        //    connect.Open();
-        //    DataTable dt_toode = new DataTable();
-        //    adapter_toode = new SqlDataAdapter("SELECT * FROM Toodetable", connect);
-        //    adapter_toode.Fill(dt_toode);
+
+        public void Tooded(int kat_Id)
+        {
+            connect.Open();
+            DataTable dt_toode = new DataTable();
+            adapter_toode = new SqlDataAdapter("SELECT * FROM Toodetable", connect);
+            adapter_toode.Fill(dt_toode);
+
+            tlp = new TableLayoutPanel();
+            foreach (DataRow toode in dt_toode.Rows)
+            {
+                //PictureBox pbox=new PictureBox.Image.Add.FromFile("../../Images/about.png");
+            }
+
+            connect.Close();
             
-        //    tlp = new TableLayoutPanel();
-        //    foreach (DataRow toode in adapter_toode)
-        //    {
-        //        //PictureBox pbox=new PictureBox.Image.Add.FromFile("../../Images/about.png");
-        //    }
-
-        //    connect.Close();
-
-        //}
+        }
+        int kat_Id;
+        string failid;
+        
+        List<string> fail_list;
+        public List<string> Failid_KatId(int kat_Id)
+        {
+            fail_list = new List<string>();
+            failinimi_adap = new SqlDataAdapter("SELECT Pilt FROM Toodetable WHERE Kategooria_Id=" + kat_Id, connect);
+            //kat_Id_adap.MissingSchemaAction = MissingSchemaAction.Add;
+            DataTable failid = new DataTable();
+            failinimi_adap.Fill(failid);
+            foreach (DataRow fail in failid.Rows)
+            {
+                fail_list.Add(fail["Pilt"].ToString());
+            }
+            return fail_list;
+        }
         public void Kategooriad()
         {
             kategooriad = new TabControl();
@@ -54,7 +73,8 @@ namespace DB_TARpv21
             kategooriad.Height = this.Height;
 
             connect.Open();
-            adapter_kat = new SqlDataAdapter("SELECT Kategooria_nimetus FROM Kategooria", connect);
+            adapter_kat = new SqlDataAdapter("SELECT Id, Kategooria_nimetus FROM Kategooria", connect);
+            
             DataTable dt_kat = new DataTable();
             adapter_kat.Fill(dt_kat);
             ImageList iconsList = new ImageList();//
@@ -68,6 +88,25 @@ namespace DB_TARpv21
                 iconsList.Images.Add(Image.FromFile(@"..\..\Kat_pildid\" + (string)nimetus["Kategooria_nimetus"] + ".jpg"));//
                 kategooriad.TabPages[i].ImageIndex = i;//
                 i++;//
+                kat_Id = (int)nimetus["Id"];
+                fail_list = Failid_KatId(kat_Id);
+                int r = 0;
+                int c = 0;
+                foreach (var fail in fail_list)
+                {
+                    MessageBox.Show(fail);
+                    pictureBox = new PictureBox();
+                    pictureBox.Image = Image.FromFile(@"..\..\Images\"+fail);
+                    pictureBox.Width = pictureBox.Height = 100;
+                    pictureBox.SizeMode=PictureBoxSizeMode.StretchImage;
+                    pictureBox.Location= new Point(r, c);
+                    r = r + 100+2;
+                    kategooriad.TabPages[i-1].Controls.Add(pictureBox);
+
+                }
+                
+                
+               
 
             }
             kategooriad.ImageList = iconsList;//
